24919d7292551a77fdde8803cdfbe349b386f5c0
Renard Burnett <renburnett@Renards-MacBook-Pro.local>
2019-01-08 17:48:05 -0800

    implemented option modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-    <div>
-        some text
-    </div>
+  <Modal
+    isOpen={!!props.selectedOption}
+    onRequestClose={props.handleClearSelectedOption}
+    contentLabel="Selected Option"
+  >
+    <h3>Selected Option</h3>
+    {props.selectedOption && <p>{props.selectedOption}</p>}
+    <button onClick={props.handleClearSelectedOption}>Okay</button>
+  </Modal>
 );
 
 export default OptionModal;
